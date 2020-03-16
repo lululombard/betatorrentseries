696ef0939d18c148c182d7bf9e208a7d67f86c67
lululombard <lululombard@gmail.com>
2020-03-16 00:50:23 +0000

    Working image test

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,6 @@ COPY .env /root
 
 RUN apk --update add bash python3 transmission-daemon && rm -rf /var/cache/apk/*
 
-RUN mkdir -p /root/downloads
-
 RUN python3 -m pip install -r /root/requirements.txt
 
 ENV START_TRANSMISSION_DAEMON=1
--- test_image.sh
+++ test_image.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+cd "${0%/*}"
 
 # Build the docker image
 docker build -t betatorrentseries .
 
 mkdir -p downloads
 
-docker run -v downloads:/root/downloads -p 9091:9091 --name betatorrentseries betatorrentseries
+docker run -v `pwd`/downloads:/root/downloads -p 9091:9091 --name betatorrentseries betatorrentseries
